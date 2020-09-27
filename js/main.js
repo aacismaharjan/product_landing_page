08e89d140b096a271135203b3ebe9bb91de991ff
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-09-27 09:47:07 +0545

    Added Testinomial Section

--- js/main.js
+++ js/main.js
@@ -19,4 +19,25 @@ $(document).ready(function () {
       },
     },
   })
+
+  // Testinomial Carousel
+  $('#testinomial-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  })
 })
